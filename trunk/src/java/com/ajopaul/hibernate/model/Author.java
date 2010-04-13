0822a95053538c565b4ec6965b0cb7eafcd09ed8
ajopaul01 <ajopaul01@424f5bb1-e9d8-705f-ab0c-6398d0439e73>
2010-04-13 09:30:17 +0000

    Updated fixes and added temp hibernate files

---
+++ trunk/src/java/com/ajopaul/hibernate/model/Author.java
@@ -0,0 +1,34 @@
+package com.ajopaul.hibernate.model;
+
+import java.util.List;
+
+
+public class Author {
+    private long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    private List<Book> books;
+}
